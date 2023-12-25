590e6802e4b109c67f9ddf4416c164e5a9cbea2f
Dawid <57856214+dawoodshahzad07@users.noreply.github.com>
2023-12-25 12:31:44 +0500

    Update nlp_app.py

--- nlp_app.py
+++ nlp_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pickle
 import re
+import nltk
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
